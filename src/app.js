da3e95e3edc13f0088aa5916880bd32914f11ecf
AdroitMachine <anujsaxenamail@gmail.com>
2020-10-22 19:23:15 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+console.log(__dirname);
+console.log(path.join(__dirname, '../public'));
+//Define paths for express config
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../templates/partials')
+//setup handlebars and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath);
+//Setup static directory to serve
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Adroit Machine'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: "Andrew Mead"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Adroit Machine'
+    })
+})
+app.use(express.static(publicDirectoryPath))
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'you must provide an address'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    // res.send({
+    //     forecast: 'it is snowing right now',
+    //     location: 'philadelphia',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'YOu must provide a search term'
+        })
+    }
+    res.send({
+        products: []
+    })
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Adroit machine',
+        errorMessage: 'Help article not found'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Adroit Machine',
+        errorMessage: 'Page not found'
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
